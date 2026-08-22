c4d7e7e706d7244b79baffe3679060f47c077d0e
Erick <erick.andresda05@gmail.com>
2026-08-21 19:05:28 -0600

    Se agrega  la función descarga CSV y TXT en Estadisticas.razor según Feature 2.4 completa

---
+++ src/LaPachangaDelMundialV2/wwwroot/descargar.js
@@ -0,0 +1,8 @@
+﻿window.descargarArchivo = function (nombreArchivo, contenido) {
+    var elemento = document.createElement('a');
+    elemento.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(contenido));
+    elemento.setAttribute('download', nombreArchivo);
+    document.body.appendChild(elemento);
+    elemento.click();
+    document.body.removeChild(elemento);
+};
